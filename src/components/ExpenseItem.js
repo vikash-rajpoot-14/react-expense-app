f130d20e4e1316f07c209f207af525d46f2033df
vikash-rajpoot-14 <vikashraj1490@gmail.com>
2023-09-06 16:14:09 +0530

    variable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import "./ExpenseItem.css"
 
 function ExpenseItem() {
-  return (
-    <div>
-      <p>expense items</p>
-    </div>
-  )
+    let date = new Date(25,1,2023)
+    console.log(date)
+    let carname = "Honda";
+    let price = 29.5;
+    let LocationOfExpenditure  = "GOA"
+    return (
+        <div>
+            <div className="expense-item">
+                <div>{carname}</div>
+                <div className="expense-item__description">
+                    <h2>{date.toISOString()}</h2>
+                    <div style={{color:'white'}}>{LocationOfExpenditure}</div>
+                    <div className="expense-item__price">${price}</div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ExpenseItem

0edc5b3ff4788406342c8ba4c35371ef4de1b0d7
vikash-rajpoot-14 <vikashraj1490@gmail.com>
2023-09-08 02:43:10 +0530

    single state

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,28 +4,33 @@ import Card from '../UI/Card'
 import "./ExpenseItem.css"
 
 function ExpenseForm() {
-   const [title,setTitle] = useState("")
-   const [amount,setAmount] = useState("")
-   const [date,setDate] = useState("")
-   const [location,setLocation] = useState("")
+    const [title, setTitle] = useState("")
+    const [amount, setAmount] = useState("")
+    const [date, setDate] = useState("")
+    const [location, setLocation] = useState("")
+    const [formData, setFormdata] = useState([])
 
-
-    const titleHandler=(e)=>{
+    const titleHandler = (e) => {
         setTitle(e.target.value)
     }
 
-    const dateHandler=(e)=>{
+    const dateHandler = (e) => {
         setDate(e.target.value)
     }
-    const locationHandler=(e)=>{
+    const locationHandler = (e) => {
         setLocation(e.target.value)
     }
-    const amountHandler=(e)=>{
+    const amountHandler = (e) => {
         setAmount(e.target.value)
     }
-    const submitHandler=(e)=>{
-      e.preventDefault()
-      console.log(title, date, amount, location)
+    const submitHandler = (e) => {
+        e.preventDefault()
+        let obj = {
+            title, date, amount, location
+        }
+        console.log(obj)
+        console.log(formData)
+        setFormdata([...formData,obj])
     }
 
 
@@ -34,23 +39,23 @@ function ExpenseForm() {
             <div className='formparent'>
                 <div className='formchild'>
                     <label>Title</label>
-                    <input value={title} onChange={(e)=>titleHandler(e)} name='title' type="text" />
+                    <input value={title} onChange={(e) => titleHandler(e)} name='title' type="text" />
                 </div>
                 <div className='formchild'>
                     <label>Amount</label>
-                    <input value={amount} onChange={(e)=>amountHandler(e)} name='amount' type="text" />
+                    <input value={amount} onChange={(e) => amountHandler(e)} name='amount' type="text" />
                 </div>
 
                 <div className='formchild'>
                     <label>Location</label>
-                    <input value={location} onChange={(e)=>locationHandler(e)} name='location' type="text" />
+                    <input value={location} onChange={(e) => locationHandler(e)} name='location' type="text" />
                 </div>
                 <div className='formchild'>
                     <label>Date</label>
-                    <input value={date} onChange={(e)=>dateHandler(e)} name='date' type="date" />
+                    <input value={date} onChange={(e) => dateHandler(e)} name='date' type="date" />
                 </div>
                 <div className='formchild'>
-                    <button onClick={e=>submitHandler(e)} type="submit">submit</button>
+                    <button onClick={e => submitHandler(e)} type="submit">submit</button>
                 </div>
             </div>
         </Card>

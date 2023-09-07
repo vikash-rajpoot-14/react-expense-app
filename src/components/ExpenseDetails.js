e84bcce64c9da6cbac818540f3f462f5d2b91e61
vikash-rajpoot-14 <vikashraj1490@gmail.com>
2023-09-08 01:30:32 +0530

    add 100

--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react'
 
 const ExpenseDetails = ({id,deleteHandler, array, title, amount, location }) => {
-    const [titles, setTitle] = useState(title)
-
-    const DeleteHandler = () => {
-        array = array.filter(ele=>ele.id !== id)
-        setTitle("updated")
-        deleteHandler(array)
+    const [amounts, setAmount] = useState(amount)
+    // console.log(titles)
+    const AddExpense = () => {
+        // array = array.filter(ele=>ele.id !== id)
+        // setTitle("updated")
+        // deleteHandler(array)
+        // console.log("after",titles)
+        setAmount(100)
       
     }
 
     return (
         <React.Fragment>
             <div className="expense-item__description">
-                <h2>{titles}</h2>
+                <h2>{title}</h2>
                 <div>{location}</div>
-                <div className="expense-item__price">${amount}</div>
-                <button onClick={(e)=>DeleteHandler()}>change title</button>
+                <div className="expense-item__price">${amounts}</div>
+                <button onClick={(e)=>AddExpense()}>change title</button>
             </div>
         </React.Fragment>
     )
